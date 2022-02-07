2fd1a9bb61afe8ce7b78bd8f0ae7acd3f6bbdad9
Hiroki Osame <hiroki.osame@gmail.com>
2022-02-07 16:35:36 -0500

    refactor: remove awaited generic

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,5 @@
 import type { Options } from 'p-map';
 
-// From: https://github.com/microsoft/TypeScript/blob/4f5b3299fee9a54b692aba9df7a9e894bd86e81d/src/lib/es2015.promise.d.ts#L1
-type Awaited<T> = (
-	T extends undefined
-		? T
-		: (
-			T extends PromiseLike<infer U>
-				? U
-				: T
-		)
-);
-
 export type TaskObject = {
 	title: string;
 	state: 'pending' | 'loading' | 'error' | 'warning' | 'success';
